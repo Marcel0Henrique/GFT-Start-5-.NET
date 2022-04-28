ec57059d8375aaea9d9b06712178a138c58ccbd0
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-04-28 12:52:10 -0300

    update

--- projetos/DigitalInnovationOne/Program.cs
+++ projetos/DigitalInnovationOne/Program.cs
@@ -2,6 +2,11 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num = 5;
+
+        for (int i = 0; i < num; i++)
+        {
+            Console.WriteLine($"Bem-vindo ao curso de .Net {i}");
+        }
     }
 }
