21a4462ad0965b1a7e4c316d0cee567116267473
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-04-28 23:23:47 -0300

    debug teste

--- projetos/EstruturaDoPrograma/Program.cs
+++ projetos/EstruturaDoPrograma/Program.cs
@@ -1,4 +1,9 @@
 ﻿internal class Program
 {
-    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Marcelo");
+        Console.WriteLine("Ester");
+        Console.WriteLine("Teste");
+    }
 }
