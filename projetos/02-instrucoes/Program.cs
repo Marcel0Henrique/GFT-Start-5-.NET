a573bccf2f84e42e19202ce46b52cb5187798bc0
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-04-30 13:22:20 -0300

    update

--- projetos/02-instrucoes/Program.cs
+++ projetos/02-instrucoes/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    static void Declaracoes(){
+
+        //* Declarando variaveis
+        int a;
+        double b = 4.7;
+        a = 10; //? atribuindo valor a variavel
+
+        //* Declarando constantes
+        const int c = 5;
+        const double pi = 3.14;
+
+        //* Declarando variavel nullabe
+        int? nula = null; //? variavel revebendo valor nulo
+
+        //* Declarando variaveis na mesma linha
+        int d = 9, e = 16, f = 18;
+
+        Console.WriteLine(a + b + c + e + d + f + pi);
+        
+        Console.WriteLine(nula.ToString());
+
+    }
+
+    static void intrucaoIf(String[] lista){
+        if (lista.Length == 0)
+        {
+            Console.WriteLine("Lista vazia");
+        }else if(lista.Length == 1){
+            Console.WriteLine("Lista tem 1 item");
+        }else
+        {
+            Console.WriteLine("Lista tem mais de 1 item");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Declaracoes();
+    }
+}
