f750af5d1962fc28cb5d4ca5567f3fc9efd7ea91
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-04-30 23:06:14 -0300

    update

--- projetos/02-instrucoes/Program.cs
+++ projetos/02-instrucoes/Program.cs
@@ -1,6 +1,7 @@
 ﻿internal class Program
 {
-    static void Declaracoes(){
+    static void Declaracoes()
+    {
 
         //* Declarando variaveis
         int a;
@@ -18,25 +19,73 @@
         int d = 9, e = 16, f = 18;
 
         Console.WriteLine(a + b + c + e + d + f + pi);
-        
+
         Console.WriteLine(nula.ToString());
 
     }
 
-    static void intrucaoIf(String[] lista){
+    static void InstrucaoIf(string[] lista)
+    {
         if (lista.Length == 0)
         {
             Console.WriteLine("Lista vazia");
-        }else if(lista.Length == 1){
+        }
+        else if (lista.Length == 1)
+        {
             Console.WriteLine("Lista tem 1 item");
-        }else
+        }
+        else
         {
             Console.WriteLine("Lista tem mais de 1 item");
         }
     }
 
-    public static void Main(string[] args)
+    static void IntrucaoSwitch(string[] args)
     {
+        int numeroDeArgumentos = args.Length;
+
+        switch (numeroDeArgumentos)
+        {
+            case 0:
+
+                Console.WriteLine("Argumentos tem 0 itens");
+                break;
+            case 1:
+                Console.WriteLine("Argumentos tem 1 item");
+                break;
+            default:
+                Console.WriteLine($"Argumentos tem {numeroDeArgumentos} itens");
+                break;
+        }
+    
+    }
+
+    static void InstrucaoWhile (string[] args)
+    {
+        int i = 0;
+        while (i < args.Length)
+        {
+            Console.WriteLine(args[i]);
+            i++;
+        }
+    }
+
+    static void InstrucaoDo()
+    {
+        string? texto;
+        do
+        {   Console.WriteLine();
+            texto = Console.ReadLine();
+            Console.WriteLine(texto);
+        } while (!string.IsNullOrEmpty(texto));
+    }
+    public static void Main(string[] args)
+    {   
+        string[] nome = {"Marcelo", "Henrique", "Monteiro"};
         Declaracoes();
+        InstrucaoIf(lista:nome);
+        IntrucaoSwitch(args:nome);
+        InstrucaoWhile(args:nome);
+        InstrucaoDo();
     }
 }
