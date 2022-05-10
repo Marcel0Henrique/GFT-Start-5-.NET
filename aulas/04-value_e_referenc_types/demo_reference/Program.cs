0d5e9c3d0d8f59a6aa22c89ba5db2286fde4e229
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-10 14:27:49 -0300

    update

--- aulas/04-value_e_referenc_types/demo_reference/Program.cs
+++ aulas/04-value_e_referenc_types/demo_reference/Program.cs
@@ -5,6 +5,10 @@ internal class Program
     private static void Main(string[] args)
     {   
         Pessoa p1 = new Pessoa("Marcos");
+        p1.Idade = 26;
+        p1.Documento = "14523";
+
+        p1.AlterarNome("Manoel");
 
         WriteLine(p1.Nome);
     }
