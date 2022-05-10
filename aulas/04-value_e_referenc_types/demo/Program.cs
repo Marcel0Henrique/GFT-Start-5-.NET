53c992d31743e20e7f80e1c5e33fd7feddf1ecc4
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-10 12:06:28 -0300

    update

--- aulas/04-value_e_referenc_types/demo/Program.cs
+++ aulas/04-value_e_referenc_types/demo/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        WriteLine("Hello, World!");
+    }
+}
