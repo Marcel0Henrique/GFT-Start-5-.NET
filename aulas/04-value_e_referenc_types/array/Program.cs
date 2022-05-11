fcef5d38bbdbe4bfef360f8487b0f034f43e3b17
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-11 14:24:36 -0300

    update

--- aulas/04-value_e_referenc_types/array/Program.cs
+++ aulas/04-value_e_referenc_types/array/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace array
+﻿using static System.Console;
+namespace array
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] pares = new int[]{0,2,4,6,8,10,12,14,16,18,20};
+
+            foreach (var num in pares)
+            {
+                WriteLine(num);
+            }
+
+            MudarParaImpar(pares);
+
+            foreach (var num in pares)
+            {
+                WriteLine(num);
+            }
+        }
+
+        static void MudarParaImpar(int[] pares)
+        {
+            for (var i = 0; i < pares.Length; i++)
+            {
+                pares[i] = pares[i] + 1;
+            }
         }
     }
 }
