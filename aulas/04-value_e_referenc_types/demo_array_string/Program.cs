3a670f11f0f406d52df0bcf6af89daa72ffa3075
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-12 14:35:17 -0300

    update

--- aulas/04-value_e_referenc_types/demo_array_string/Program.cs
+++ aulas/04-value_e_referenc_types/demo_array_string/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using static System.Console;
+internal class Program
 {
     private static void Main(string[] args)
     {
         string[] nomes = new string[]{"Marcelo", "Maria", "Ester", "Clara", "John"};
+        AlterarNome(nomes,"Clara","Rodrigo");
+        foreach (var nome in nomes)
+        {
+            WriteLine(nome);
+        }
+
+    }
+
+    static void AlterarNome(string[] lista, string nomeAntigo,string nomeNovo)
+    {
+        for (var i = 0; i < lista.Length; i++)
+        {
+            if (lista[i] == nomeAntigo)
+            {
+                lista[i] = nomeNovo;
+            }
+        }
     }
 }
