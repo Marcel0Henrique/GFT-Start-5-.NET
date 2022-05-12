3bfe14c12417caad914f25e6646fe6f04dc35585
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-12 10:30:46 -0300

    update[

--- aulas/04-value_e_referenc_types/desen_algoritmo/Program.cs
+++ aulas/04-value_e_referenc_types/desen_algoritmo/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using static System.Console;
+internal class Program
 {
     private static void Main(string[] args)
     {
+        int[] numeros = new int[]{0,2,4,6,8};
+        Write($"Digite o número que vc gostaria de encontrar:");
+        int num = int.Parse(ReadLine());
+
+        int idxEncontrado = EncontrarNumero(numeros, num);
+        WriteLine($"Posição do numero encontrado: {idxEncontrado}");
         
+    }
+
+    static int EncontrarNumero(int[] numeros, int numero)
+    {
+        for (var i = 0; i < numeros.Length; i++)
+        {
+            if (numeros[i] == numero)
+            {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
