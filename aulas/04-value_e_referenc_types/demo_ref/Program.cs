caf0dae3788aed4f9e7f6daef89c6e6c5d0fc953
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-12 14:00:52 -0300

    update

--- aulas/04-value_e_referenc_types/demo_ref/Program.cs
+++ aulas/04-value_e_referenc_types/demo_ref/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using static System.Console;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n = 5;
+        Adicionar20(ref n);
+        WriteLine($"{n}");
+
+
+    }
+
+    static void Adicionar20(ref int num)
+    {
+        num += 20;
     }
 }
