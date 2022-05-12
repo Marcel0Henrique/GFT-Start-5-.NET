744899056f20928ed460240350ca71b7fab4d1b5
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-12 16:08:27 -0300

    update

--- desafios/desafio_media/Program.cs
+++ desafios/desafio_media/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {   
+        const double pesoNota1 = 2, pesoNota2 = 3, pesoNota3 = 5;
+        const double pesoMedia  = pesoNota1 + pesoNota2 + pesoNota3;
+        double nota1,nota2,nota3, media;
+
+        nota1 = double.Parse(ReadLine());
+        nota2 = double.Parse(ReadLine());
+        nota3 = double.Parse(ReadLine());
+
+        nota1 = nota1 * pesoNota1;
+        nota2 = nota2 * pesoNota2;
+        nota3 = nota3 * pesoNota3;
+        media = (nota1 + nota2 + nota3) / pesoMedia;
+        
+        
+        WriteLine($"MEDIA = {media.ToString("0.0")}");
+            
+        
+
+
+    }
+}
