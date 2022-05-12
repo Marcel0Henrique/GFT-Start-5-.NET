514fbb7abf41be4e0377f1727d0f5640c44d4a36
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-12 14:56:12 -0300

    update

--- aulas/04-value_e_referenc_types/demo_array_string/Program.cs
+++ aulas/04-value_e_referenc_types/demo_array_string/Program.cs
@@ -3,23 +3,43 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        string[] nomes = new string[]{"Marcelo", "Maria", "Ester", "Clara", "John"};
-        AlterarNome(nomes,"Clara","Rodrigo");
-        foreach (var nome in nomes)
-        {
-            WriteLine(nome);
-        }
+        string[] nomes = new string[] { "Marcelo", "Maria", "Ester", "Clara", "John" };
+
+
+
+        //? Imprimindo a lista de nomes
+        WriteLine($"***Lista de nomes***\n{string.Join(", ", nomes)}");
+
+        int index = LocalizaNome(nomes, "Ester");
+
+        AlterarNome(nomes, index, "Caio");
+
+        WriteLine($"\n***Lista de nomes alterada***\n{string.Join(", ", nomes)}");
 
     }
 
-    static void AlterarNome(string[] lista, string nomeAntigo,string nomeNovo)
+    static int LocalizaNome(string[] lista, string nome)
     {
-        for (var i = 0; i < lista.Length; i++)
+        for (int i = 0; i <= lista.Length; i++)
         {
-            if (lista[i] == nomeAntigo)
+            if (lista[i] == nome)
             {
-                lista[i] = nomeNovo;
+                return i;
             }
+        }
+
+        return -1;
+    }
+
+    static void AlterarNome(string[] lista, int index, string nome)
+    {
+        try
+        {
+            lista[index] = nome;
+        }
+        catch (System.Exception ex)
+        {
+            WriteLine($"Não foi possível alterar o nome\n Erro:{ex}");
         }
     }
 }
