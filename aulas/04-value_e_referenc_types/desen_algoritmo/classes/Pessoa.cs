3c6fdd3cd2a6e3103d070a204d25b39a9380433e
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-12 10:11:47 -0300

    update

---
+++ aulas/04-value_e_referenc_types/desen_algoritmo/classes/Pessoa.cs
@@ -0,0 +1,14 @@
+namespace desen_algoritmo.classes
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+        }
+
+        public string? Nome { get; set; }
+        public int idade { get; set; }
+        
+        
+    }
+}
