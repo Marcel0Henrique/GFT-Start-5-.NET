328cd1cb8560897219d6d6907c62d1edfb0fe39d
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-13 15:54:48 -0300

    update

--- desafio de projetos/jogo_rpg/src/Classes/Personagem2.cs
+++ desafio de projetos/jogo_rpg/src/Classes/Personagem2.cs
@@ -2,13 +2,17 @@ namespace jogo_rpg.src.Classes
 {
     public class Personagem2
     {
-        public Personagem2(string name, int level = 1, int hp = 100, int mp = 20)
+        public Personagem2(string name, int level = 1, int hp = 100, int mp = 20, int strength = 10, int agility = 5, int intelligence = 5, int Constitution = 10 )
         {
             this.Name = name;
             this.Level = level;
             this.Hp = hp;
             this.Mp = mp;
             this.Classe = Classes.Warrior;
+            this.Strength = strength;
+            this.Agility = agility;
+            this.Intelligence = intelligence;
+            this.Constitution = Constitution;
         }
 
         public string Name { get; set; }
@@ -16,6 +20,10 @@ namespace jogo_rpg.src.Classes
         public int Level { get; set; }
         public int Hp { get; set; }
         public int Mp { get; set; }
+        public int Strength { get; set; }
+        public int Agility { get; set; }
+        public int Intelligence { get; set; }
+        public int Constitution { get; set; }
         
         Classes Classe;
         
@@ -29,7 +37,17 @@ namespace jogo_rpg.src.Classes
         };
 
         
-        
+        public virtual int PhysicalAttack(Personagem2 inimigo)
+        {
+            if (Classe == Classes.Warrior)
+            {
+                //* Usar media poderada para calcular o ataque
+                return 2;
+            }
+
+            return 2;
+            
+        }
 
     }
 }
