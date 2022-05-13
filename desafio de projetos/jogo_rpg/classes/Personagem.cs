8cf7c112feb9adc8f8611d40779c6437e3dc65b5
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-13 10:09:08 -0300

    update

---
+++ desafio de projetos/jogo_rpg/classes/Personagem.cs
@@ -0,0 +1,11 @@
+namespace jogo_rpg.classes
+{
+    public class Personagem
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Classe { get; set; }
+        
+        
+    }
+}
