799b2c7cc3193d8171a06f2c0549860d37abb998
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-14 11:56:26 -0300

    update

--- desafios/volume_esfera/Program.cs
+++ desafios/volume_esfera/Program.cs
@@ -1,11 +1,13 @@
 ﻿using static System.Console;
+using static System.Math;
+using static System.Convert;
 internal class Program
 {
     private static void Main(string[] args)
     {
         const double pi = 3.14159;
-        double r = Convert.ToDouble(ReadLine());
-        double volume = ((4 * pi) * Math.Pow(r,3.0)) / 3;
+        double r = ToDouble(ReadLine());
+        double volume = ((4 * pi) * Pow(r,3.0)) / 3;
 
         WriteLine($"VOLUME = {volume.ToString("0.000")}");
     }
