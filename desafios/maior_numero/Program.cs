f7eb6d175d8a61d62673ee16bae23512a1d4c723
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-14 12:14:45 -0300

    update

--- desafios/maior_numero/Program.cs
+++ desafios/maior_numero/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+using static System.Convert;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+            int maior = 0;
+            int posicao = 0;
+            for(int i = 1; i <= 100 ; i++)
+            {
+                n = ToInt32(Console.ReadLine());
+                if(n > maior)
+                {
+                    maior = n;
+                    posicao = i;     
+                } 
+            }
+            WriteLine(maior);
+            WriteLine(posicao);
+    }
+}
