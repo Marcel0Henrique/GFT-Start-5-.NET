0fc8260b744fc6dad03f827fefb5e9322fefb140
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-14 12:07:14 -0300

    update

---
+++ desafios/poligono_regular/Program.cs
@@ -0,0 +1,11 @@
+﻿using static System.Console;
+using static System.Convert;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] v = ReadLine().Split();
+        int p = ToInt32(v[0]) * ToInt32(v[1]);
+        WriteLine($"{p}");
+    }
+}
