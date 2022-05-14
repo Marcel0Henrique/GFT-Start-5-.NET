285719a04b68cd49d0ac1b7449a89390ae7cdb45
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-14 11:53:13 -0300

    update

--- desafios/volume_esfera/Program.cs
+++ desafios/volume_esfera/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const double pi = 3.14159;
+        double r = Convert.ToDouble(ReadLine());
+        double volume = ((4 * pi) * Math.Pow(r,3.0)) / 3;
+
+        WriteLine($"VOLUME = {volume.ToString("0.000")}");
+    }
+}
