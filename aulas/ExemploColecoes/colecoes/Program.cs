d6c3ac5b695a63ab142b1de49e34a6d3d13daa1c
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-17 14:08:13 -0300

    update

--- aulas/ExemploColecoes/colecoes/Program.cs
+++ aulas/ExemploColecoes/colecoes/Program.cs
@@ -2,6 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        int[] arrayInteiros = new int[]{1,2,3,4};
+        int[] arrayInteiros = new int[3];
+        arrayInteiros[0] = 1;
+        arrayInteiros[1] = 2;
+        arrayInteiros[2] = 3;
+
+        foreach (var item in arrayInteiros)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
