504af47a20bb2a6a2c8684d560900cf8f563876d
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-23 16:55:24 -0300

    update

--- aulas/linq/Program.cs
+++ aulas/linq/Program.cs
@@ -2,6 +2,13 @@
 {
     private static void Main(string[] args)
     {
-        
+        int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
+        //* Sintaxe de consulta
+        var numerosPares =
+            from num in numeros
+            where num % 2 == 0
+            orderby num
+            select num;
     }
 }
