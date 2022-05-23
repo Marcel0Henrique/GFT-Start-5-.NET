1f03f2ec6fbe90406e94e261aaafd7d24a94725f
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-23 20:42:14 -0300

    interfacesExemple

---
+++ aulas/interfaces/interfaces/ICalculadora.cs
@@ -0,0 +1,17 @@
+namespace interfaces.src.classes
+{
+    public interface ICalculadora
+    {
+         void somar()
+         {}
+
+         void diminuir()
+         {}
+
+         void multiplicar()
+         {}
+
+         void dividir()
+         {}
+    }
+}
